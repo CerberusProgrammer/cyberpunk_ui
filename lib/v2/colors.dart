c14c3a1fadf60b7174083e307b66fcd6c1875919
sazardev <sazardev@gmail.com>
2024-10-10 19:15:18 -0700

    Add Cyberpunk color palette

---
+++ lib/v2/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+const cyberColors = {
+  'RED': Color.fromARGB(255, 247, 79, 73),
+  'CYAN': Color.fromARGB(255, 94, 247, 255),
+  'WHITE': Color.fromARGB(255, 255, 255, 255),
+  'DARKEST BLUE': Color.fromARGB(255, 14, 14, 23),
+  'GREY': Color.fromARGB(255, 214, 208, 208),
+  'GREEN (MINT JUNGLE)': Color.fromARGB(255, 29, 237, 131),
+  'BLUE (YALE STEEL)': Color.fromARGB(255, 37, 112, 212),
+  'VIOLET PURPLE': Color.fromARGB(255, 157, 43, 245),
+  'ORANGE': Color.fromARGB(255, 251, 147, 46),
+  'YELLOW (GOLD)': Color.fromARGB(255, 241, 181, 55),
+};
