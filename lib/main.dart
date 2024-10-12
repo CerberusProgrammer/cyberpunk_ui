14b339da5ec3fea73be8df4ab1867042c1e76625
sazardev <sazardev@gmail.com>
2024-10-11 21:53:00 -0700

    Refactor button components to include custom color properties and support different border types

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:cyberpunk_ui/v2/button/filled/cyber_filled_button.dart';
+import 'package:cyberpunk_ui/v2/button/filled/cyber_filled_button_shape.dart';
+import 'package:cyberpunk_ui/v2/button/text/cyber_text_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,17 +18,44 @@ class MainApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: Scaffold(
-        body: Column(
-          children: [
-            CyberFilledButton(text: 'a', onTap: () {}),
-            CyberFilledButton(text: 'aa', onTap: () {}),
-            CyberFilledButton(text: 'aaa', onTap: () {}),
-            CyberFilledButton(text: 'aaaaaaaaa', onTap: () {}),
-            CyberFilledButton(
-                icon: Icons.ac_unit, text: 'aceptar', onTap: () {}),
-            CyberFilledButton(
-                icon: Icons.ac_unit, text: 'aceptar', onTap: () {}),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CyberFilledButton(
+                    icon: Icons.ac_unit,
+                    text: 'cancelar',
+                    onTap: () {},
+                    borderType: CyberBorderType.left,
+                  ),
+                  CyberFilledButton(
+                    icon: Icons.ac_unit,
+                    text: 'aceptar',
+                    onTap: () {},
+                    borderType: CyberBorderType.right,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  CyberTextButton(
+                    icon: Icons.ac_unit,
+                    text: 'cancelar',
+                    onTap: () {},
+                    borderType: CyberBorderType.left,
+                  ),
+                  CyberTextButton(
+                    icon: Icons.ac_unit,
+                    text: 'aceptar',
+                    onTap: () {},
+                    borderType: CyberBorderType.right,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
